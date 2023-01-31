9f317ae994d863fc66e63aa219098134fd2db94b
Anton <antonch92@gmail.com>
2023-01-31 23:59:26 +0100

    iterator kaputt

--- src/js/team.js
+++ src/js/team.js
@@ -1,8 +1,55 @@
 export default class Team {
-    constructor(...initialHeroes) {
-      this.members = new Set(initialHeroes);
-      console.log(this.members);
+  constructor(...initialHeroes) {
+    this.members = new Set(initialHeroes);
+  }
+
+  [Symbol.iterator]() {
+    let counter = 0;
+    return {
+      next() {
+        if (counter <= this.members.size) {
+          counter++;
+          const arr = [];
+          this.members.forEach((hero) => arr.push(hero));
+          return {
+            done: false,
+            value: arr[counter - 1].type
+          }
+        };
+
+        return {
+          done: true
+        };
+      }
     }
+  }
+}
 
+const anton = new Team ({
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10
+},
+{
+  name: 'Лучник',
+  type: 'Anton',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10
+},
+{
+  name: 'Лучник',
+  type: 'Vlad',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10
+});
 
+for (let item of anton) {
+  console.log(item);
 }
